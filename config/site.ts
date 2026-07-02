56e323412329185c2b854a884a0c7de1b1dfa53a
cowok jantan <85976240+cowokjantan@users.noreply.github.com>
2026-07-02 08:44:02 +0700

    Update site.ts

--- config/site.ts
+++ config/site.ts
@@ -1,1 +1,13 @@
-
+export const SITE_CONFIG = {
+  token: {
+    name: "RobinBull",
+    ticker: "RBULL",
+    contractAddress: "0x0f495aa6339e7df924dd69524e35c3de2ce4693b",
+  },
+  links: {
+    dexScreenerEmbed: "https://dexscreener.com/robinhood/0x0f495aa6339e7df924dd69524e35c3de2ce4693b?embed=1&theme=dark&trades=0&info=0",
+    tradingView: "https://www.tradingview.com",
+    twitter: "https://x.com/RobinBull",
+    telegram: "https://t.me/RobinBull",
+  }
+};
