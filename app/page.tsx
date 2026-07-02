0b47b9bc76010670559a3c8859a8e23d38d465b2
cowok jantan <85976240+cowokjantan@users.noreply.github.com>
2026-07-02 08:46:08 +0700

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -1,1 +1,17 @@
+import Navbar from "@/components/Navbar";
+import Hero from "@/components/Hero";
+import ChartSection from "@/components/ChartSection";
+import Tokenomics from "@/components/Tokenomics";
 
+export default function Home() {
+  return (
+    <>
+      <Navbar />
+      <main>
+        <Hero />
+        <ChartSection />
+        <Tokenomics />
+      </main>
+    </>
+  );
+}
