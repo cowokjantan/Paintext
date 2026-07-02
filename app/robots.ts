c4fefde958042e9666100bedea802317e40cd6d2
cowok jantan <85976240+cowokjantan@users.noreply.github.com>
2026-07-02 08:37:42 +0700

    Delete app directory

--- app/robots.ts
+++
@@ -1,9 +0,0 @@
-import type { MetadataRoute } from "next";
-import { SITE_CONFIG } from "@/config/site";
-
-export default function robots(): MetadataRoute.Robots {
-  return {
-    rules: { userAgent: "*", allow: "/" },
-    sitemap: `${SITE_CONFIG.seo.url}/sitemap.xml`,
-  };
-}
