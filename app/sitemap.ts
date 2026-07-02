9e82c270e76b791d5e1d00f784920b5ba0a1a10f
cowok jantan <85976240+cowokjantan@users.noreply.github.com>
2026-07-02 08:13:11 +0700

    Update sitemap.ts

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,1 +1,13 @@
+import type { MetadataRoute } from "next";
+import { SITE_CONFIG } from "@/config/site";
 
+export default function sitemap(): MetadataRoute.Sitemap {
+  return [
+    {
+      url: SITE_CONFIG.seo.url,
+      lastModified: new Date(),
+      changeFrequency: "daily",
+      priority: 1,
+    },
+  ];
+}
